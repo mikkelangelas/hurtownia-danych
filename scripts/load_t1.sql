9c520fda4f53fb7700561c91f036b59583f721b1
Stanisław Grochowski <grochowski0stanislaw@gmail.com>
2024-11-30 22:24:29 +0100

    scripts for dw, db creation, bulk load and data

---
+++ scripts/load_t1.sql
@@ -0,0 +1,8 @@
+USE RentMaster;
+
+BULK INSERT dbo.Pracownik from 'dane\pracownik1.csv' WITH (FIELDTERMINATOR=',');
+BULK INSERT dbo.Marka from 'dane\marka.csv' WITH (FIELDTERMINATOR=',');
+BULK INSERT dbo.Punkt from 'dane\punkt.csv' WITH (FIELDTERMINATOR=',');
+BULK INSERT dbo.ElementWyposazenia from 'dane\element_wyposazenia1.csv' WITH (FIELDTERMINATOR=',');
+BULK INSERT dbo.Wypozyczenie from 'dane\wypozyczenie1.csv' WITH (FIELDTERMINATOR=',');
+BULK INSERT dbo.WypozyczenieWyposazenia from 'dane\wypozyczenie_wyposazenia1.csv' WITH (FIELDTERMINATOR=',');
